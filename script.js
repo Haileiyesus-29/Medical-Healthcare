436ba9c6e30e91b976862c5e72eb91105b9e0399
Haileiyesus <119631162+Haileiyesus-29@users.noreply.github.com>
2023-01-24 13:04:56 +0300

    Major changes of the website
    added new styles and new pages of the website.
    and introduce some JavaScript file on the sign up page.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-
+const genderRadios = document.querySelectorAll(".gender-labels-hidden");
+const genderInputs = document.querySelectorAll(".gender-inputs");
+const loginForm = document.querySelector(".login-form");
+const employeeRadio = document.querySelector(".signup-employee #employee");
+const employeeLabel = document.querySelector(".employee-label");
+const employeeCheckbox = document.querySelector(
+  ".signup-employee .checkbox-ball"
+);
+genderInputs.forEach((input) =>
+  input.addEventListener("change", () => {
+    genderRadios.forEach((el) => el.classList.toggle("radio-checked"));
+  })
+);
+employeeRadio.addEventListener("change", () => {
+  employeeLabel.classList.toggle("active");
+  employeeCheckbox.classList.toggle("active");
+});
